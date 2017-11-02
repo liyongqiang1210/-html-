873104e47ef96fbef73908605cc020a2a11f4d24
liyongqiang1210 <1240965061@qq.com>
2017-11-02 09:00:22 +0800

    layui版本更新

--- layui-admin/js/nav.js
+++ layui-admin/js/nav.js
@@ -4,39 +4,65 @@ var navs = [{
 	"href" : "page/main.html",
 	"spread" : false
 },{
-	"title" : "文章列表",
+	"title" : "系统设置",
 	"icon" : "icon-text",
 	"href" : "page/news/newsList.html",
 	"spread" : false
 },{
-	"title" : "友情链接",
+	"title" : "用户管理",
 	"icon" : "icon-text",
 	"href" : "page/links/linksList.html",
 	"spread" : false
 },{
-	"title" : "404页面",
+	"title" : "权限管理",
 	"icon" : "&#xe61c;",
 	"href" : "page/404.html",
 	"spread" : false
 },{
-	"title" : "系统基本参数",
+	"title" : "新闻数据管理",
 	"icon" : "&#xe631;",
 	"href" : "page/systemParameter/systemParameter.html",
-	"spread" : false
-},{
-	"title" : "二级菜单演示",
-	"icon" : "&#xe61c;",
-	"href" : "",
 	"spread" : false,
 	"children" : [
 		{
-			"title" : "二级菜单1",
+			"title" : "国际新闻",
 			"icon" : "&#xe631;",
 			"href" : "",
 			"spread" : false
 		},
 		{
-			"title" : "二级菜单2",
+			"title" : "国内新闻",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},
+		{
+			"title" : "军事新闻",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},
+		{
+			"title" : "体育新闻",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		}
+	]
+},{
+	"title" : "博客数据管理",
+	"icon" : "&#xe61c;",
+	"href" : "",
+	"spread" : false,
+	"children" : [
+		{
+			"title" : "后端技术",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},
+		{
+			"title" : "前端技术",
 			"icon" : "&#xe631;",
 			"href" : "",
 			"spread" : false
